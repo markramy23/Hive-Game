92d63cccfc77232ebc839bfcbfc38e9146a10b09
markramy23 <2000923@eng.asu.edu.eg>
2024-11-25 22:31:55 +0200

    Constants and available position function
    added constants to be used in for hexagons drawing and a function to get the available positions for a piece

--- Hive_Game.py
+++ Hive_Game.py
@@ -1,1 +1,60 @@
+import pygame
+import math
 
+# Constants
+WIDTH = 800
+HEIGHT = 600
+HEX_SIZE = 30  # Size of the hexagon
+HEX_COLOR = (255, 255, 255)  # White (Fill Color)
+BORDER_COLOR = (0, 0, 0)  # Black (Border Color)
+SELECTED_COLOR = (255, 0, 0)  # Red
+NEIGHBOR_COLOR = (0, 255, 0)  # Green
+BACKGROUND_COLOR = (0, 0, 0)  # Black
+def AvailablePositions(hex_map, turn):
+    result = []
+    s = set();
+    if turn == "W":
+        if hex_map.Length == 0:
+            return True
+        elif hex_map.Length ==1 :
+            keys = list(hex_map.map.keys())
+            return  hex_map.get_Empty_neighbors(keys[0][0],keys[0][1] )
+        else:
+            keys = list(hex_map.map.keys())
+            List = []
+            for key in keys:
+                List += hex_map.get_Empty_neighbors(key[0], key[1])
+                for element in List:
+                    s.add((element[0], element[1]))
+
+            for element in s:
+                flag = 0
+                for element2 in List:
+                    if (element[0] == element2[0]) and (element[1] == element2[1]) and element2[2] == "B":
+                        flag = 1
+                if flag == 0:
+                    result.append((element[0], element[1]))
+
+    if turn == "B":
+        if hex_map.Length == 0:
+            return True
+        elif hex_map.Length == 1:
+            keys = list(hex_map.map.keys())
+            return hex_map.get_Empty_neighbors(keys[0][0], keys[0][1])
+        else:
+            keys = list(hex_map.map.keys())
+            List = []
+            for key in keys:
+                List += hex_map.get_Empty_neighbors(key[0], key[1])
+                for element in List:
+                    s.add((element[0], element[1]))
+
+            for element in s:
+                flag = 0
+                for element2 in List:
+                    if (element[0] == element2[0]) and (element[1] == element2[1]) and (element2[2] == "W"):
+                        flag = 1
+                if flag == 0:
+                    result.append((element[0], element[1]))
+
+    return result

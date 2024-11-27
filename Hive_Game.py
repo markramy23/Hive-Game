f20507649d5b77366d4e1b6538cc13b4b1fd34a7
seifalrahman <130805731+seifalrahman@users.noreply.github.com>
2024-11-27 02:56:30 +0200

    Added Function to  handle the Moves of the Beetle

--- Hive_Game.py
+++ Hive_Game.py
@@ -180,6 +180,7 @@ class HexMap:
         self.Black_turn_count = 0
         self.Turn = "W"
         self.Length = 0
+        self.OutCasts=[]
 
     def add_piece(self, q, r, name, color):
         self.Length += 1
@@ -206,6 +207,15 @@ class HexMap:
         """Returns a list of neighboring hexes for the given hex."""
         directions = [(+1, 0), (-1, 0), (0, +1), (0, -1), (+1, -1), (-1, +1)]
         return [(q + dq, r + dr) for dq, dr in directions if (q + dq, r + dr) in self.map]
+    
+    def move_beetle(self,q,r,new_q,new_r):
+        if (new_q,new_r) in self.map.keys() :
+            self.OutCasts.append(  ( (new_q,new_r) , self.map[(new_q, new_r)] ) )
+        self.map[(new_q, new_r)] = self.map.pop((q, r))
+        for OutCast in self.OutCasts :
+            if OutCast[0] == (q,r) :
+                self.map[OutCast[0]]=OutCast[1]
+
 
     def get_Empty_neighbors(self, q, r):
         """Returns a list of neighboring hexes for the given hex."""

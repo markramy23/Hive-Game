3305fc1ebac2b70bbe125018ac9db3b35def844a
FawzyBashandy <131293631+FawzyBashandy@users.noreply.github.com>
2024-11-27 21:44:29 +0200

    Adding GUI related procedure

--- Hive_Game.py
+++ Hive_Game.py
@@ -423,6 +423,29 @@ def draw_single_hex(q, r, camera_x, camera_y, screen, hex_map, selected_hex):
     if piece:
         draw_text_centered(screen, piece, x, y)
 
+def draw_player(player1_name,player2_name, Players_type,screen):
+    
+    # Get the window size after setting the display mode
+    window_width, window_height = pygame.display.get_surface().get_size()
+    player_background = pygame.image.load("Player_background.png")
+    screen.blit(player_background,(0,0))
+    screen.blit(player_background,(window_width-250,0))
+    num_of_pieces = 11 #for one player
+
+    #White Player
+    name1 = font.render(player1_name,True,BLACK)
+    screen.blit(whiteIcon,(window_width-195,10))
+    screen.blit(name1, (window_width-170,10))
+    
+    #Black Player
+    name2 = font.render(player2_name,True,BLACK)
+    screen.blit(blackIcon,(55,10))
+    screen.blit(name2, (80,10))
+
+    #if Players_type is "Human-Human":    
+    #elif Players_type is "Human-AI":
+    #elif Players_type is "AI-AI":
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
